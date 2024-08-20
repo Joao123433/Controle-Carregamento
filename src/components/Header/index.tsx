9c2b16db80f38f0b3a3d917c017e1051170666e6
Joao123433 <joaosantos123433@gmail.com>
2024-08-19 23:46:04 -0300

    Atualizacao do README

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -3,13 +3,12 @@ import { Dropdown } from "flowbite-react"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import { faFile } from "@fortawesome/free-solid-svg-icons"
 
-
 export default function Header() {
   return (
     <header className="flex flex-row justify-between w-full h-20 py-3 items-center gap-4">
       <Link href={"/"} className="flex flex-row items-center gap-2">
         <FontAwesomeIcon icon={faFile} size="lg" style={{color: "#ffffff",}} />
-        <h1 className="text-slate-50 text-2xl device:text-xl">Carregamento Coluna</h1>
+        <h1 className="text-slate-50 text-2xl device:text-xl">Carregamento</h1>
       </Link>
       <nav className="flex flex-row justify-between gap-8 text-white text-lg device:h-full device:flex-col device:justify-center device:gap-1 device:items-center device:text-base">
         <Dropdown label="Fora Cidade" inline className="p-4 bg-zinc-900 rounded-lg">
